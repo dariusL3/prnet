2c09a62aedfa3f2d24259e052a6325e2c38407c5
Dat LE <datlt@blockchainlabs.asia>
2018-09-28 14:43:19 +0700

    path fix

--- fullscript.sh
+++ fullscript.sh
@@ -6,11 +6,11 @@ tar -xvzf go1.10.2.linux-amd64.tar.gz
 # set PATH environment variable
 export GOPATH=/home/quocltm/go/goprojects
 export GOROOT=/home/quocltm/go
-export PATH=/home/quocltm/go/bin
+go=/home/quocltm/go/bin/go
 # clone custom private net
 mkdir $GOPATH
 mkdir $GOPATH/src
 mkdir $GOPATH/src/github.com
 mkdir $GOPATH/src/github.com/dat96le
-go get github.com/dat96le/bin
+$go get github.com/dat96le/bin
 cd $GOPATH/src/github.com/dat96le/bin
