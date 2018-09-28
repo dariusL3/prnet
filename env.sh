d4e0c6519ea069a4bde8a4629453b486512df535
Dat LE <datlt@blockchainlabs.asia>
2018-09-28 14:27:47 +0700

    path fix

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 export GOPATH=/home/quocltm/go/goprojects
 export GOROOT=/home/quocltm/go
-export PATH=$PATH:/home/quocltm/go/bin
+export PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/geth/:/home/quocltm/.local/bin:/home/quocltm/bin:/home/quocltm/go/bin
--- fullscript.sh
+++ fullscript.sh
@@ -6,7 +6,7 @@ tar -xvzf go1.10.2.linux-amd64.tar.gz
 # set PATH environment variable
 export GOPATH=/home/quocltm/go/goprojects
 export GOROOT=/home/quocltm/go
-export PATH=$PATH:/home/quocltm/go/bin
+# export PATH=$PATH:/home/quocltm/go/bin
 # clone custom private net
 mkdir $GOPATH
 mkdir $GOPATH/src
